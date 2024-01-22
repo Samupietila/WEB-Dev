4759312ba2ed8513b7c4bb9c5f770ea1139755fa
Samu <samupie@metropolia.fi>
2024-01-22 11:39:47 +0200

    oops

--- week-2/react-conditional-jsx-lab/src/UserGreeting.js
+++ week-2/react-conditional-jsx-lab/src/UserGreeting.js
@@ -1,16 +1,14 @@
 function UserGreeting(props) {
     const isLoggedIn = props.isLoggedIn;
 
-    let greetingContent;
-    if (isLoggedIn) {
-        greetingContent = <h1>Welcome back, User!</h1>;
-    } else {
-        greetingContent = <h1>Please log in to continue</h1>;
-    }
-
+    // Conditional rendering using the ternary operator
     return (
         <div>
-            {greetingContent}
+               {isLoggedIn ? (
+                   <h1>Welcome back, User!</h1>
+               ) : (
+                   <h1>Please log in to continue</h1>
+               )}
         </div>
     );
 }
