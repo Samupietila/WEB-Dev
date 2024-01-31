e51bd98b77f5a20113bd6333574f97169888302b
Samu <samupie@metropolia.fi>
2024-01-31 13:36:56 +0200

    week3 activities backend

---
+++ week-3/Backend/activity1-MVC/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('./middleware/logger');
+const { unknownEndpoint, errorHandler } = require('./middleware/errorMiddleware');
+
+const app = express();
+
+// Init middleware
+app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+
+// Members API Routes
+app.use('/api/members', require('./routers/membersRouter'));
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
