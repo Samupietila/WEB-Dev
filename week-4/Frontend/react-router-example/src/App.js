a62e3f8a168472fe76b649274fe63bb8fa9a2ec5
Samu <samupie@metropolia.fi>
2024-02-07 10:46:09 +0200

    Bakcend activity-1

--- week-4/Frontend/react-router-example/src/App.js
+++ week-4/Frontend/react-router-example/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
